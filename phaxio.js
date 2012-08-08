8de14dbd189011b7dcad74dfbffd773bcd326b40
Francis Gulotta <wizard@roborooter.com>
2012-08-08 00:20:25 -0400

    start of rewrite

--- phaxio.js
+++ phaxio.js
@@ -4,82 +4,54 @@ var util = require('util'),
   fs = require('fs'),
   request = require('request');
 
-var Phaxio = function(api_key, api_secret) {
-  this.api_key = api_key || null;
-  this.api_secret = api_secret || null;
+var Phaxio = module.exports = function(api_key, api_secret) {
+  this.api_key = api_key;
+  this.api_secret = api_secret;
   this.host = 'https://api.phaxio.com';
   this.endpoint = '/v1';
 };
 
-Phaxio.prototype = {
-  getApiKey: function() {
-    return this.api_key;
-  },
-  getApiSecret: function() {
-    return this.api_secret;
-  },
-  setApiKey: function(api_key) {
-    if(!api_key)
-      throw new Error('You must include an API key.');
-    this.api_key = api_key;
-    return this;
-  },
-  setApiSecret: function(api_secret) {
-    if(!api_secret)
-      throw new Error('You must include an API secret.');
-    this.api_secret = api_secret;
-    return this;
-  },
-  faxStatus: function(faxId, callback) {
-    if(!faxId)
-      throw new Error('You must include a fax id.');
-    return this.doRequest('/faxStatus', { id: faxId  }, callback);
-  },
-  sendFax: function(to, filenames, params, callback) {
-    if('[object Function]' == Object.prototype.toString.call(params)) {
-      callback = params;
-      params = {};
-    }
-    if('[object Object]' == Object.prototype.toString.call(filenames)) {
-      params = filenames;
-      filenames = [];
-    }
-    to = util.isArray(to) ? to : [to];
-    filenames = util.isArray(filenames) ? filenames : [filenames];
-    if(!to)
-      throw new Error("You must include a 'to' number.");
-    if(!filenames.length && !params.string_data)
-      throw new Error('You must include a file to send.');
-    params = this.paramsCopy(['string_data', 'string_data_type', 'batch', 'batch_delay', 'callback_url'], params || {});
-    params.to = to;
-    params.filename = filenames;
-    return this.doRequest('/send', params, callback);
-  },
-  fireBatch: function(batchId, callback) {
-    if(!batchId)
-      throw new Error('You must provide a batchId.');
-    return this.doRequest('/fireBatch', { id: batchId }, callback);
-  },
-  closeBatch: function(batchId, callback) {
-    if(!batchId)
-      throw new Error('You must provide a batchId.');
-    return this.doRequest('/closeBatch', { id: batchId }, callback);
-  },
-  provisionNumber: function(area_code, callback_url, callback) {
-    if(!area_code)
-      throw new Error('You must provide an area code.');
-    var params = { area_code: area_code };
-    if('[object Function]' == Object.prototype.toString.call(callback_url))
-      callback = callback_url;
-    else
-      params.callback_url = callback;
-    this.doRequest('/provisionNumber', params, callback);
-  },
-  releaseNumber: function(number, callback) {
-    if(!number)
-      throw new Error('You must provide a number.');
-    return this.doRequest('/releaseNumber', { number: number }, callback);
-  },
+Phaxio.prototype.faxStatus = function(faxId, callback) {
+  if(!faxId) { throw new Error('You must include a fax id.'); }
+  return this.doRequest('/faxStatus', { id: faxId  }, callback);
+};
+
+Phaxio.prototype.sendFax = function(opt, callback) {
+  if( !opt.to ) { throw new Error("You must include a 'to' number."); }
+  if( !opt.filenames && !opt.string_data ){
+    throw new Error("You must include filenames or string_data.");
+  }
+  return this.doRequest('/send', opt, callback);
+};
+
+Phaxio.prototype.fireBatch = function(batchId, callback) {
+  if( !batchId ) { throw new Error('You must provide a batchId.'); }
+  return this.doRequest('/fireBatch', { id: batchId }, callback);
+};
+
+Phaxio.prototype.closeBatch = function(batchId, callback) {
+  if( !batchId ) { throw new Error('You must provide a batchId.'); }
+  return this.doRequest('/closeBatch', { id: batchId }, callback);
+};
+
+
+Phaxio.prototype.provisionNumber =  function(area_code, callback_url, callback) {
+  if( !area_code ) { throw new Error('You must provide an area code.'); }
+  var params = { area_code: area_code };
+  if( !callback ) {
+    callback = callback_url;
+  } else {
+    params.callback_url = callback_url;
+  }
+  this.doRequest('/provisionNumber', params, callback);
+};
+
+Phaxio.prototype.releaseNumber = function(number, callback) {
+  if(!number) { throw new Error('You must provide a number.'); }
+  return this.doRequest('/releaseNumber', { number: number }, callback);
+};
+
+var bogus = {
   numberList: function(params, callback) {
     if('[object Function]' == Object.prototype.toString.call(params))
       callback = params;
@@ -186,13 +158,5 @@ Phaxio.prototype = {
       callback && callback(body);
     });
     return this;
-  },
-  paramsCopy: function(names, options) {
-    for(var params = {}, k = 0, l = names.length; k < l; k++)
-      if(options[names[k]])
-        params[names[k]] = options[names[k]];
-    return params;
   }
 };
-
-module.exports = Phaxio;
