366ede6154f10aaf554946e30e4c4f55178df75a
Bojan Djurkovic <dbojan@gmail.com>
2013-03-22 10:47:38 -0300

    set max sockets

--- phaxio.js
+++ phaxio.js
@@ -140,10 +140,11 @@ Phaxio.prototype.request = function(resource, opt, cb) {
     headers: { 'content-type': 'multipart/form-data;' },
     multipart: multipart,
     encoding: 'binary',
-    agent: false 
+    pool.maxSockets: 10
   };
 
   var responceCb = function(err, res, body) {
+    res.on('close', function () {response.emit('end')});
     // phaxio isn't too picky about response types
     if(res && (true || res.headers['content-type'] === 'application/json')){
       try{
